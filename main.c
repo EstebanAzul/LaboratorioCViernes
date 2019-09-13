cf09321504d2e18cda1283248c5692bcd71a307b
Aula211-08 <estebanasul@gmail.com>
2019-09-13 08:55:17 -0500

    Pracitca6

--- main.c
+++ main.c
@@ -1,8 +1,53 @@
+/* 
+* Autor: Esteban Carvajal Villanueva
+* Fecha: 13/09/2019
+* grupo: 006
+* Horario: 7am-9am
+* Matrícula: 1882011
+*/
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+int main () {
+	
+	//**************ACTIVIDAD1*****************
+		printf("      COMIENZA PROGRAMA 1     \n");
+	int x = 0, z = 0;
+	char y;
+	printf("Programa que recibe valores hasta que el usuario\nhaya terminado de ingresar numeros y al final\nimprime la suma de todos los valores ingresados\n\n");
+	printf("Ingrese un numero entero: ");
+	scanf("%d", &x);
+	z = x;
+	x = 0;
+	fflush(stdin);
+	printf("\nDesea ingresar otro valor? (s/n) :");
+	scanf("%c", &y);
+	while(y == 's'){
+		fflush(stdin);
+		printf("\nIngrese un numero entero: ");
+		scanf("%d", &x);
+		z = z + x;
+		x = 0;
+		fflush(stdin);
+		printf("\nDesea ingresar otro valor? (s/n) :");	
+		scanf("%c", &y);
+	}
+	
+	printf("\nLa suma de todos los valores es: %d",z);
+	
+	//**************ACTIVIDAD2*****************
+	printf("\n\n\n\n\n      COMIENZA PROGRAMA 2     ");
+	int a = 0;
+	printf("\n\n\nPrograma que recibe valores hasta que el numero ingresado sea mayor o igual a 100\n\n");
+	printf("Ingrese un numero entero: ");
+	scanf("%d", &x);
+	printf("%d",x);
+	fflush(stdin);
+	while(x < 100){
+		fflush(stdin);
+		printf("\n\nIngrese un numero entero: ");
+		scanf("%d", &x);
+		printf("\n%d",x);
+	}
+	
 }
