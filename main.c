cbc4b165c6b314c139aeab950184c96ed0834f76
EstebanAzul <estebanasul@gmail.com>
2019-09-20 08:38:26 -0500

    Practica7

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
+/*
+*Autor: Esteban Carvajal Villanueva
+*Fecha: 20/09/2019
+*grupo: 006
+*Horario: 7am-9am
+*Matricula: 1882011
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
+//**************ACTIVIDAD1*****************
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main () {
+	
+	
+	int i;
+	float prom = 0, tot = 0, cal = 0;
+	printf("Programa que dadas 10 calificaciones de un alumno regresa el promedio (calificacion base 10)\n");
+	for(i = 1; i <= 10; i++){
+		printf("\nIngrese la calificacion %d del alumno ", i);
+		scanf("%f", &cal);
+		tot = tot + cal;
+	}
+	prom = tot/10;
+	if(prom >= 7){
+	printf("\nEl alumno aprobo el curso con la calificacion de: %.2f", prom);
+}
+else {
+	printf("\nEl alumno reprobo el curso con la calificacion de %.2f", prom);
+}
+//**************ACTIVIDAD2*****************
 
-int main(int argc, char *argv[]) {
-	return 0;
+int base = 0, exponente = 0, var = 0, resultado = 0, j = 0;
+
+printf("\n\n\n\n\n Programa que dados base y exponente, calcula el resultado\n");
+printf("\nIngrese la base ");
+scanf("%d", &base);
+printf("\nIngrese el exponente ");
+scanf("%d", &exponente);
+var = base;
+for(j = 2; j <= exponente; j++){
+	var *= base;
+}
+printf("\n\nEl resultado es: %d", var);
 }
